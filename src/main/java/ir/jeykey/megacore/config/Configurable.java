56fce4d2d4f1e77f25496ab203bb11fee4993ccf
Alijk <alijafarkazemi@gmail.com>
2022-01-06 00:50:48 +0330

    add premade storage config

--- src/main/java/ir/jeykey/megacore/config/Configurable.java
+++ src/main/java/ir/jeykey/megacore/config/Configurable.java
@@ -2,6 +2,7 @@ package ir.jeykey.megacore.config;
 
 import ir.jeykey.megacore.MegaPlugin;
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
